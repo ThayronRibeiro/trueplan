363b2e33df2f07cf423092d7918b403bfac71338
Graco <thayron.21@gmail.com>
2024-03-29 22:23:11 -0300

    test commit

---
+++ src/main/java/com/graco/trueplan/entity/Usuario.java
@@ -0,0 +1,69 @@
+package com.graco.trueplan.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+@JsonRootName(value = "usuario")
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome", length = 100)
+	private String nome;
+	
+	@Column(name = "email", length = 100)
+	private String email;
+	
+	@Column(name = "senha")
+	private String senha;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	@Column(name = "data_cadastro")
+	private LocalDateTime dataCadastro;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+	@Column(name = "ultimo_acesso")
+	private LocalDateTime ultimoAcesso;
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + "]";
+	}
+	
+	
+	
+	
+
+}
