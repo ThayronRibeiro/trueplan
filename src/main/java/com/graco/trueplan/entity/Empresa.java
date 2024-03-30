d492f1e4be9a7270719169e7e03c2f39ab2316a7
Graco <thayron.21@gmail.com>
2024-03-29 22:25:00 -0300

    test commit

---
+++ src/main/java/com/graco/trueplan/entity/Empresa.java
@@ -0,0 +1,14 @@
+package com.graco.trueplan.entity;
+
+import java.io.Serializable;
+
+public class Empresa implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+
+}
