551ede49de371756fec91b390a4b7462e589ca72
Graco <thayron.21@gmail.com>
2024-03-29 22:04:16 -0300

    first commit

---
+++ src/main/java/com/graco/trueplan/TrueplanApplication.java
@@ -0,0 +1,13 @@
+package com.graco.trueplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrueplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrueplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/graco/trueplan/TrueplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graco.trueplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
