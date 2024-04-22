790db158b59b2c7df33453d8b1762ecf9b98ceb8
Graco <thayron.21@gmail.com>
2024-04-22 17:18:54 -0300

    changes in repository

--- src/main/java/com/graco/trueplan/repository/ChamadoRepository.java
+++ src/main/java/com/graco/trueplan/repository/ChamadoRepository.java
@@ -10,10 +10,10 @@ import com.graco.trueplan.entity.Chamado;
 
 public interface ChamadoRepository extends JpaRepository<Chamado, Long>{
 
-	@Query(value = "select to_char(cast(data_abertura as date), 'DD-MM-YYYY') as data from chamados group by cast(data_abertura as date)\r\n"
-			+ "			order by cast(data_abertura as date) DESC", nativeQuery = true)
+	@Query(value = "select to_char(cast(data_chamado as date), 'DD-MM-YYYY') as data from chamados group by cast(data_chamado as date)\r\n"
+			+ "			order by cast(data_chamado as date) DESC", nativeQuery = true)
 	public List<String> selectDataAbertura();
 	
-	@Query(value = "select * from chamados where cast(data_abertura as date) = cast(:date as date) order by id ASC", nativeQuery = true)
+	@Query(value = "select * from chamados where cast(data_chamado as date) = cast(:date as date) order by id ASC", nativeQuery = true)
 	public List<Chamado> selectByDataAbertura(Date date);
 }
