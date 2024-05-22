9fd953f81862b8a33ff16ab159de89142de758ae
Graco <thayron.21@gmail.com>
2024-05-22 17:46:14 -0300

    generic repository

--- src/main/java/com/graco/trueplan/repository/CargoRepository.java
+++ src/main/java/com/graco/trueplan/repository/CargoRepository.java
@@ -1,9 +1,7 @@
 package com.graco.trueplan.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.graco.trueplan.entity.Cargo;
 
-public interface CargoRepository extends JpaRepository<Cargo, Long>{
+public interface CargoRepository extends GenericRepository<Cargo, Long>{
 
 }
